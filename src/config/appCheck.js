14f519429499479b5106a344315a5fb4e9d6758e
Naum <stojanovski.nano@gmail.com>
2026-02-12 17:10:20 +0100

    Remove unused imports

--- src/config/appCheck.js
+++ src/config/appCheck.js
@@ -1,7 +1,3 @@
-import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
-import { app } from './firebase';
-
 export const initAppCheck = () => {
-  // Disable App Check for now since no reCAPTCHA key is set
-  return null;
+  // App Check disabled
 };
